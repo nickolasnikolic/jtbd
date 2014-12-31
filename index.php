e006b20e39293131b09107286f0205d8a551cc8b
Nickolas Nikolic <nickolasnikolic@gmail.com>
2014-12-31 07:05:49 -0600

    small change: favicon on landing page; meta tags;

--- index.php
+++ index.php
@@ -6,8 +6,10 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="">
-    <meta name="author" content="">
+    <meta name="description" content="Landing page for a small JTBD web app">
+    <meta name="author" content="Nickolas Nikolic">
+    
+    <link rel="icon" type="image/png" href="/app/images/favicon.png">
 
     <title>jtbd.rocks landing page</title>
 
